162cac67dacc96092bef1f8d2f5517c9e22ac534
Paul Hlusko <paul.hlusko@gmail.com>
2022-08-10 09:16:07 -0400

    work work

--- Scripts/PlayerControls.cs
+++ Scripts/PlayerControls.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerControls : MonoBehaviour
-{
+public class PlayerControls : MonoBehaviour {
+    public float speed = 0;
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        Debug.Log("Initialize player rigid body");
+        rb = GetComponent<Rigidbody>();
+    } 
+
+    void FixedUpdate() {
+        Debug.Log("moving player");
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+        if (rb.GetComponent<Vector3>().y < 3) {
+            rb.MovePosition(new Vector3(0,0.5f,0));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnMove(InputValue movementValue) {
+        Debug.Log("capturing input");
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
     }
 }
