0ceed9d316647c735621c0df0210ea3710ff2e13
Paul Hlusko <paul.hlusko@gmail.com>
2022-08-16 15:43:35 -0400

    tute done

--- Scripts/PlayerControls.cs
+++ Scripts/PlayerControls.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerControls : MonoBehaviour {
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winText;
     private Rigidbody rb;
     private float movementX;
     private float movementY;
+    private int count;
 
     // Start is called before the first frame update
     void Start() {
         Debug.Log("Initialize player rigid body");
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.SetActive(false);
     } 
 
     void FixedUpdate() {
@@ -28,9 +35,19 @@ public class PlayerControls : MonoBehaviour {
         movementY = movementVector.y;
     }
 
+    void SetCountText(){
+        countText.text = "Count: " + count.ToString();
+        if (count == 14) {
+            countText.gameObject.SetActive(false);
+            winText.SetActive(true);
+        }
+    }
+
     private void OnTriggerEnter(Collider other) {
         if(other.gameObject.CompareTag("PickUp")) {
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
         }
     }
 }
